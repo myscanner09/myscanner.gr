ed8c5849280be4632f7aa1e84b19b1dd0bae01a5
myscanner09 <myscanner09@gmail.com>
2026-05-17 12:21:06 +0300

    Add Google OAuth config

--- photomatch/app/config.py
+++ photomatch/app/config.py
@@ -17,6 +17,9 @@ class Settings:
     
     GOOGLE_DRIVE_PARENT_FOLDER_ID: str = os.getenv("GOOGLE_DRIVE_PARENT_FOLDER_ID", "")
     GOOGLE_SERVICE_ACCOUNT_JSON: str = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON", "")
+    GOOGLE_CLIENT_ID: str = os.getenv("GOOGLE_CLIENT_ID", "")
+    GOOGLE_CLIENT_SECRET: str = os.getenv("GOOGLE_CLIENT_SECRET", "")
+    GOOGLE_REFRESH_TOKEN: str = os.getenv("GOOGLE_REFRESH_TOKEN", "")
 
 
 
